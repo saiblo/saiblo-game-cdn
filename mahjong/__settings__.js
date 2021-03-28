14a7ea010764e68093b6a788ad1f84ca75b25237
UNIDY2002 <UNIDY2002@outlook.com>
2021-03-28 23:56:28 +0800

    feat: mahjong (naive)

---
+++ mahjong/__settings__.js
@@ -0,0 +1,21 @@
+ASSET_PREFIX = "https://player.dev.saiblo.net/mahjong/";
+SCRIPT_PREFIX = "https://player.dev.saiblo.net/mahjong/";
+SCENE_PATH = "https://player.dev.saiblo.net/mahjong/1116708.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 44850199, 44747522, 44893598, 44893602, 44921856, 44967261, 44919245, 44925897, 44910141, 45013005, 45013008, 45013006, 45013007, 45048425 ];
+CONFIG_FILENAME = "https://player.dev.saiblo.net/mahjong/config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+];
