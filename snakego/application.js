90aa768acafd707361b4d17532ba4ff35e81736b
李旭浚 <lixujun20@mails.tsinghua.edu.cn>
2022-01-21 00:01:20 +0800

    fix snake link bug

--- snakego/application.js
+++ snakego/application.js
@@ -167,7 +167,7 @@ System.register([], function (_export, _context) {
     var launchScene = settings.launchScene; // load scene
 
     cc.director.loadScene(launchScene, null, function () {
-      cc.view.setDesignResolutionSize(960, 640, 4);
+      cc.view.setDesignResolutionSize(960, 640, 2);
       console.log("Success to load scene: ".concat(launchScene));
     });
   }
