992843ca1eb178d191fff5aac88a9da530b70f7f
UNIDY2002 <UNIDY2002@outlook.com>
2022-04-06 00:00:19 +0800

    Snow cup

---
+++ snow-cup/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/helloworld.fire",orientation:"",jsList:[]};
